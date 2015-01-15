1637ccfa9a5e35c4ebcbe0509b8010590546ea46
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-15 22:34:55 +0100

    Fixed test

--- test.js
+++ test.js
@@ -3,12 +3,10 @@ var test 			= require('ava');
 var cripNames = require('./');
 
 test(function (t) {
-	t.assert(cripNames.crip.length > 0);
 	t.assert(cripNames.all.length > 0);
-	t.assert(cripNames.cripRandom());
 	t.assert(cripNames.allRandom());
 	t.assert(cripNames.allRandom() !== cripNames.allRandom());
-	t.assert(cripNames.crip[0] === 'Johnie Hjelm');
-	t.assert(cripNames.crip[1] === 'Viktor Johansson');
+	t.assert(cripNames.all[0] === 'Johnie Hjelm');
+	t.assert(cripNames.all[1] === 'Viktor Johansson');
 	t.end();
 });
