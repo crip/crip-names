32d46a4982f5f75f26eec745f70c1de66429d5f6
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-15 22:21:17 +0100

    Refactored

--- index.js
+++ index.js
@@ -7,23 +7,10 @@
 var uniqueRandom 	= require('unique-random');
 var cripNames 		= require('./crip-names.json');
 
+var allRandom = uniqueRandom(0, cripNames.length, -1);
 
-var allCripNames = [];
-
-cripNames.forEach(function (el, i) {
-	allCripNames.push(el, cripNames[i]);
-});
-
-var cripRandom = uniqueRandom(0, cripNames.length, -1);
-var allRandom = uniqueRandom(0, allCripNames.length, -1);
-
-exports.crip = cripNames;
-exports.all = allCripNames;
-
-exports.cripRandom = function () {
-	return cripNames[cripRandom()];
-};
+exports.all = cripNames;
 
 exports.allRandom = function () {
-	return allCripNames[allRandom()];
+	return cripNames[allRandom()];
 };
